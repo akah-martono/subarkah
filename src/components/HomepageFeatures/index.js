aa89c73fbd6ef6f4e2e483d45e5a8260f2d47824
akah-martono <143365979+akah-martono@users.noreply.github.com>
2025-04-28 06:17:08 +0700

    update featured list

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'WordPress Tutorial',
+    Svg: require('@site/static/img/subarkah-wordpress-tutorial.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Tutorial seputar WordPress, theme, plugin, optimasi dan custom.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Sys Admin Tutorial',
+    Svg: require('@site/static/img/subarkah-server-tutorial.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Tutorial seputar setup server, hosting, WP-CLI, dan optimasi server.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Blogging',
+    Svg: require('@site/static/img/subarkah-blog.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Tulisan dan opini pribadi, silahkan baca tapi jangan diambil hati.
       </>
     ),
   },
@@ -38,7 +35,7 @@ const FeatureList = [
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
+      <div className="text--center padding-vert--lg padding-horiz--xl">
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
